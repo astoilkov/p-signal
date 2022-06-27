b86a700f64c6db3b25f119452f1b47cba2b26736
Antonio Stoilkov <hello@astoilkov.com>
2022-06-27 19:43:15 +0300

    ✅ initial tests

--- test.ts
+++ test.ts
@@ -1,3 +1,45 @@
-describe('{{data.name}}', () => {
+/* eslint-env jest */
 
+import { isAbortError, pSignal } from '.'
+
+describe('p-signal', () => {
+    it('aborted before it started', async () => {
+        const controller = new AbortController()
+
+        controller.abort()
+
+        try {
+            await pSignal(controller.signal, wait())
+        } catch (err) {
+            expect(isAbortError(err)).toBe(true)
+        }
+    })
+
+    it('aborted after it started', async () => {
+        const controller = new AbortController()
+
+        const promise = pSignal(controller.signal, wait())
+
+        controller.abort()
+
+        try {
+            await promise
+        } catch (err) {
+            expect(isAbortError(err)).toBe(true)
+        }
+    })
+
+    it('promise successfully resolved when not aborted', async () => {
+        const controller = new AbortController()
+
+        await expect(async () => await pSignal(controller.signal, wait())).not.toThrow()
+    })
+
+    it('allow undefined as "signal" value', async () => {
+        await pSignal(undefined, wait())
+    })
 })
+
+function wait() {
+    return new Promise((resolve) => setTimeout(resolve, 1))
+}
